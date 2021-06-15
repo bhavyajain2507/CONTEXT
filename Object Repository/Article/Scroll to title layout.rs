115727bbf89ed3c55b11c290765927dcade991d9
Ninestack <Ninestack@DESKTOP-19NV1TD>
2021-06-15 15:43:15 +0530

    Latest 15-06-2021

---
+++ Object Repository/Article/Scroll to title layout.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Scroll to title layout</name>
+   <tag></tag>
+   <elementGuidId>5ec759af-301f-44a8-ae3f-8b35ed7155c9</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id = 'title-component']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>title-component</value>
+   </webElementProperties>
+</WebElementEntity>
